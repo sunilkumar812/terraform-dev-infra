95d363d0b344500cdf82bddbe3ce3458f6267eba
sunilkumar812 <57595813+sunilkumar812@users.noreply.github.com>
2026-06-21 08:52:18 +0530

    Create S3 bucket resource in main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
